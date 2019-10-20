02d0c8e30e5638eca0d3ba0791116bb815cfe810
DaniHdez <dannyha07@gmail.com>
2019-10-19 19:09:34 -0600

    Aeropuerto API-CRUD completo

--- TECPlane/api/controllers/aeropuertoController.js
+++ TECPlane/api/controllers/aeropuertoController.js
@@ -1,27 +1,43 @@
-var mongoose = require('mongoose');
-aeropuerto = mongoose.model('Aeropuerto');
+var mongoose = require("mongoose");
+aeropuerto = mongoose.model("Aeropuerto");
 
 exports.lista_aeropuertos = function(req, res) {
-    aeropuerto.find({}, function(error, lista){
-        if(error)
-            res.send(error);
-        res.json(lista);
-    })
-}
+  aeropuerto.find({}, function(error, lista) {
+    if (error) res.send(error);
+    res.json(lista);
+  });
+};
 
 exports.crear_nuevo = function(req, res) {
-    var new_port = new aeropuerto(req.body);
-    new_port.save(function( error, aeropuerto) {
-        if(error)
-            res.send(error);
-        res.json(aeropuerto)
-    });
+  var new_port = new aeropuerto(req.body);
+  new_port.save(function(error, aeropuerto) {
+    if (error) res.send(error);
+    res.json(aeropuerto);
+  });
 };
 
 exports.obtener_info = function(req, res) {
-    aeropuerto.findById(req.params.idAeropuerto, function(error, aeropuerto){
-        if(error)
-            res.send(error);
-        res.json(aeropuerto);
-    });
-}
+  aeropuerto.findById(req.params.codigo, function(error, aeropuerto) {
+    if (error) res.send(error);
+    res.json(aeropuerto);
+  });
+};
+
+exports.actualizar = function(req, res) {
+  aeropuerto.findOneAndUpdate(
+    { Codigo: req.params.codigo },
+    req.body,
+    { new: true },
+    function(err, aeropuerto) {
+      if (err) res.send(err);
+      res.json(aeropuerto);
+    }
+  );
+};
+
+exports.eliminar = function(req, res) {
+  aeropuerto.remove({ Codigo: req.params.codigo }, function(err, aeropuerto) {
+    if (err) res.send(err);
+    res.json({ message: "Aeropuerto eliminado correctamente" });
+  });
+};
--- TECPlane/api/routes/tecplaneRoutes.js
+++ TECPlane/api/routes/tecplaneRoutes.js
@@ -5,24 +5,17 @@ module.exports = function(app) {
   // ##################################
   // RUTAS AEROPUERTO                 #
   // ##################################
-  app
-    .route("/aeropuertos/")
-    .get(aeropuerto.lista_aeropuertos)
-  
-  app
-    .route("/aeropuerto/")
-    .post(aeropuerto.crear_nuevo);
+  app.route("/aeropuertos/").get(aeropuerto.lista_aeropuertos);
+
+  app.route("/aeropuerto/").post(aeropuerto.crear_nuevo);
 
   app
-    .route("/aeropuerto/:idAeropuerto")
+    .route("/aeropuerto/:codigo")
     .get(aeropuerto.obtener_info)
-    //.put(aeropuerto.actualizar)
-    //.delete(aeropuerto.eliminar);
+    .put(aeropuerto.actualizar)
+    .delete(aeropuerto.eliminar);
 
-    // Agregar rutas con consultas especiales (si hay)
-    // a .route("aeropuerto/idAeropuerto/<nombre consulta>") un aeropuerto especifico
-    // a .route("aeropuerto/<nombre consulta>") para todos los aeropuertos
-
-
-
+  // Agregar rutas con consultas especiales (si hay)
+  // a .route("aeropuerto/idAeropuerto/<nombre consulta>") un aeropuerto especifico
+  // a .route("aeropuerto/<nombre consulta>") para todos los aeropuertos
 };
